84763c5257326e02564256944d8292feb0b95cd7
paul fears <paulrfears@gmail.com>
2022-03-08 01:48:00 -0600

    progress?

--- index.js
+++ index.js
@@ -28,13 +28,12 @@ function _base64ToArrayBuffer(base64) {
 }
 
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
-  console.log(algo);
   let account = AccountFromSeed(_base64ToArrayBuffer(JSON.stringify(await getPrivateKey())));
   let account_mnemonic = algo.secretKeyToMnemonic(account.sk);
-
+  const baseUrl = "https://algorand-api-node.paulfears.repl.co"
   switch (requestObject.method) {
     case 'getBalance': {
-      let balance = await fetch('https://algorand-api-node.paulfears.repl.co/balance?address='+account.addr);
+      let balance = await fetch(baseUrl+"/balance?address="+account.addr);
       balance = Number(await balance.text());
       wallet.request({
         method: 'snap_confirm',
@@ -60,6 +59,35 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
           }
         ],
       });
+    case 'transfer':
+      console.log("here")
+      console.log(baseUrl)
+      let params = await fetch(baseUrl+'/getTransactionArgs')
+      params = await params.json();
+      console.log("params: ")
+      console.log(params)
+      
+      const receiver = requestObject.to;
+      const amount = requestObject.amount;
+      console.log("receiver")
+      console.log(receiver)
+      console.log("amount")
+      console.log(amount)
+      console.log(typeof params)
+      let txn = algo.makePaymentTxnWithSuggestedParamsFromObject({
+        from: account.addr, 
+        to: receiver, 
+        amount: amount,  
+        suggestedParams: params
+      });
+      console.log(txn);
+      let signedTxn = txn.signTxn(account.sk);
+      signedTxn = JSON.stringify(signedTxn);
+      fetch(baseUrl+"/broadcast", {
+        method: 'POST',
+        body: signedTxn
+      })
+      let txId = txn.txID().toString();
     default:
       throw new Error('Method not found.');
   }
