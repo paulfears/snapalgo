1289649764d2b22f4e91fedec050da83008eeea8
paul fears <paulrfears@gmail.com>
2022-03-10 17:58:19 -0600

    small change

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
         method: 'snap_confirm',
         params: [
           {
-            prompt: `your mnemonic`,
+            prompt: "your mnemonic",
             description: account.addr,
             textAreaContent:
               account_mnemonic
